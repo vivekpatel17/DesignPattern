ed33388997bade88a477da912e4a21cc96d0923d
Vivek Kumar Patel <vivek.patel@medibuddy.in>
2025-12-18 20:04:33 +0530

    CompositePattern

---
+++ structural-pattern/CompositePattern/CompositePattern.java
@@ -0,0 +1,9 @@
+package CompositePattern;
+
+public class CompositePattern {
+    
+    public static void main(String[] args) {
+        System.out.println("Composite pattern demons tration");
+        
+    }
+}

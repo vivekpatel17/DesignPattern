534f40bd5ae73ca425a49d114807de3589524a93
vivekpatel17 <vivekpatelk0@gmail.com>
2025-12-29 23:45:27 +0530

    visitor-pattern-2-implementation

--- behaviourial-pattern/VisitorPatter/VisitorPattern2.java
+++ behaviourial-pattern/VisitorPatter/VisitorPattern2.java
@@ -1,7 +1,21 @@
 package VisitorPatter;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class VisitorPattern2 {
     public static void main(String[] args) {
-        
+        InsuranceMessageVisitor visitor = new InsuranceMessageVisitor();
+
+        String name = "name";
+        String address = "address";
+        String number = "number";
+        String insuranceClass = "gold";
+
+        AbstractClient bank = new Bank(name, address, number, insuranceClass);
+        AbstractClient resident = new Resident(name, address, number, insuranceClass);
+
+        List<AbstractClient> clients = Arrays.asList(bank, resident);
+        visitor.sendInsuranceMails(clients);
     }
 }
